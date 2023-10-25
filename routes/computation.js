1ff1728a1277ea2d2891b72c6eb2b47f873e2a77
Ramakotireddy9505 <rkotireddyragipindi4b6@gmail.com>
2023-10-24 19:18:33 -0500

    bonus

--- routes/computation.js
+++ routes/computation.js
@@ -1,40 +1,38 @@
-const express = require("express");
-const router = express.Router();
-
+var express = require("express");
+var router = express.Router();
 router.get("/", function (req, res, next) {
+ var randNumArr = Math.random() * 2;
+ var rndNumArr = Math.round(randNumArr);
 
-  let x = req.query.x;
-  if (!x) {
-    x = Math.random() * 10;
-  }
-
-  let result;
-  let functionName;
-
-  switch (lastDigitOfID % 3) {
-    case 0:
-      result = Math.atan(x, 2);
-      functionName = "Math.atan";
-      break;
-    case 1:
-      result = Math.exp(x);
-      functionName = "Math.exp";
-      break;
-    case 2:
-      result = Math.expm1(x);
-      functionName = "Math.expm1";
-      break;
-    default:
-      result = 'Invalid last digit';
-      functionName = 'Unknown function';
-  }
-
-  const response1 = `Function ${functionName} applied to x: ${x} the result is ${result.toFixed(3)}`;
+var arr = [Math.atan(), Math.exp(x), Math.expm1(x)];
+  let response1 = `
+  Function ${funObj[rndNumArr]} applied to x: ${x} the result is ${arr[rndNumArr].toFixed(3)}`;
   res.send(response1);
+
+ if (req.query.x) {
+  let x = req.query.x;
+  var arr = [Math.atan(), Math.exp(x), Math.expm1(x)];
+  var funObj = {
+   0: "Math.atan()",
+   1: "Math.exp()",
+   2: "Math.expm1()",
+  };
+
+ }
+ else {
+  var randNumArr = Math.random() * 2;
+  var rndNumArr = Math.round(randNumArr);
+  var randNum = Math.random() * 10;
+  var x = Math.round(randNum);
+  var funObj = {
+    0: "Math.atan()",
+    1: "Math.exp()",
+    2: "Math.expm1()",
+  };
+  var arr = [Math.atan(), Math.exp(x), Math.expm1(x)];
+  let response = `
+  Function ${funObj[rndNumArr]} applied to the random value for x from 1 to 10 is: ${x} and the result is ${arr[rndNumArr].toFixed(3)}`
+  res.send(response);
+ }
 });
-
-module.exports = router;
-
-
-
-
+module.exports = router;
